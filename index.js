773d0116e52cdca57d662e89f063b16143d094d5
Jonathan Kingston <jonathan.kingston@insight.com>
2012-10-30 10:02:20 +0000

    Fixing for browserify again, railway portion breaks things

--- index.js
+++ index.js
@@ -7,15 +7,6 @@ exports.Schema = require('./lib/schema').Schema;
 exports.AbstractClass = require('./lib/abstract-class').AbstractClass;
 exports.Validatable = require('./lib/validatable').Validatable;
 
-exports.init = function (rw) {
-    if (global.railway) {
-        railway.orm = exports;
-    } else {
-        rw.orm = {Schema: exports.Schema, AbstractClass: exports.AbstractClass};
-    }
-    require('./lib/railway')(rw);
-};
-
 try {
     if (process.versions.node < '0.6') {
         exports.version = JSON.parse(fs.readFileSync(__dirname + '/package.json')).version;
